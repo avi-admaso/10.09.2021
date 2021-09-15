e5491ca0401ec206749eb514552332b251802535
avi-admaso <aviadmaso@gmail.com>
2021-09-15 12:04:29 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,83 @@
+
+
+// var array = ["as","asd","sa","ds","aqw","eqew"]
+// array.forEach(function(item,i){
+//     console.log(i,item   )
+// })
+
+// שאלה 2
+// var array = [1,2,,3,4,5,6,7]
+// array.forEach(function(item){
+//     if (item%2==0){
+//         console.log("yes")}
+//         else
+//             console.log("no") 
+
+// })
+// שאלה 1
+//     var report = [
+//         {headline : "bla bla",
+//         writer : "avi",
+//     date : 2010},
+//         {headline : "bla asd",
+//         writer : "dani",
+//     date : 2001},
+//         {headline : "bladsa",
+//         writer : "gadi",
+//     date : 2009},
+//         {headline : "bladsaa",
+//         writer : "shimi",
+//     date : 2020}
+//     ]
+//     report.forEach(function(item){
+//         console.log(item)
+        
+//     });
+//     for(var items of report){
+//     console.log(items);
+// }
+// שאלה 2
+// let person=[{name:"avi",age:26,},
+// {name:"dani",age:38,},
+// {name:"gadi",age:25,},];
+
+// for(var i=0;i<person.length;i++){
+//     for(var key in person[i]){
+//             console.log(key +" "+person[i][key] );
+// }};
+// שאלה 3
+//     var books = [
+//         {name : "bla bla",
+//         writer : "avi",
+// price: 120},
+//         {name : "bla asd",
+//         writer : "dani",
+// price: 180},
+//         {name : "bladsa",
+//         writer : "gadi",
+// price: 140},
+//         {name : "bladsaa",
+//         writer : "shimi",
+// price: 130}
+//     ]
+//     books.forEach(function(item){
+//                 console.log(item)
+                
+//             });
+//             books.forEach(function(item){
+//                 console.log(item.writer)
+                
+//             });
+// שאלה 4
+
+// arrayNums = [
+//     1,2,3,4,5,6,7,8
+// ]
+// var sum = 0
+// arrayNums.forEach(function(item) {
+// sum = sum+item   
+// });console.log(sum)
+
+    // for(var item of arrayNums){
+    //     sum = sum+item }
+    // console.log(sum);
